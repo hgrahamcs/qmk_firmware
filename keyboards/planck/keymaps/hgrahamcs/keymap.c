b1417b259c34762c30b18919060bf854ad88c5e4
hgrahamcs <henry@hgrahamcs.com>
2021-02-28 16:51:54 -0600

    fixed missing _ in Adjust on planck:hgrahamcs

--- keyboards/planck/keymaps/hgrahamcs/keymap.c
+++ keyboards/planck/keymaps/hgrahamcs/keymap.c
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       RESET, _______, _______,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
     _______, _______,  MU_MOD, AU_ON,   AU_OFF,  AG_NORM, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT, _______,  _______,
     _______, _______, _______, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    DEBUG, _______, _______, _______, _______, _______, _______, ______, QWERTY, DVORAK, PLOVER, _______
+    DEBUG, _______, _______, _______, _______, _______, _______, _______, QWERTY, DVORAK, PLOVER, _______
 )
 
 };
